9c94a4a6c63db0d2d2a7e36c2ef11b481dd8b250
Mathieu <mathieu.locquet@hotmail.com>
2023-12-07 20:02:42 +0100

    python file toegevoegd

---
+++ oefening2.py
@@ -0,0 +1,7 @@
+getal1 = int(input("Geef eerste getal in"))
+getal2 = int(input("Geef een tweede getal in"))
+
+som = getal1 + getal2
+product = getal1*getal2
+print("De som is ",som)
+print("Het prodcut is ", product)
