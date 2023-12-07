a2ac2efcf48a7f05d2ed5f42399d5e62a1d2081a
Mathieu <mathieu.locquet@hotmail.com>
2023-12-07 20:07:53 +0100

    edit in python file

--- oefening2.py
+++ oefening2.py
@@ -1,5 +1,5 @@
-getal1 = int(input("Geef eerste getal in"))
-getal2 = int(input("Geef een tweede getal in"))
+getal1 = int(input("Geef eerste getal in "))
+getal2 = int(input("Geef een tweede getal in "))
 
 som = getal1 + getal2
 product = getal1*getal2
